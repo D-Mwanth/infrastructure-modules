45be2eea85863d7c97d9eaa53073fb77d603d4e8
D-Mwanth <dmwanthi2@gmail.com>
2024-09-11 06:07:02 +0000

    Commit k8s-addons module

---
+++ k8s-addons/autoscaler.tf
@@ -0,0 +1,97 @@
+resource "aws_iam_role" "cluster_autoscaler" {
+  name = "${var.env}-${var.eks_name}-cluster-autoscaler"
+
+  assume_role_policy = jsonencode ({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Create a policy allowing access to ec2 autoscaling api
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name = "${var.env}-cluster-autoscaler"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:DescribeTags",
+          "ec2:DescribeImages",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:GetInstanceTypesFromInstanceRequirements",
+          "eks:DescribeNodegroup"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+# attach the policy to aim role for the autoscaler
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  policy_arn = aws_iam_policy.cluster_autoscaler.arn
+  role       = aws_iam_role.cluster_autoscaler.name
+}
+
+# Associate IAM role the with kubernetes service account
+# we are providing the namespace where the autoscaler will be running and the kubernetes service account name.
+resource "aws_eks_pod_identity_association" "cluster_autoscaler" {
+  cluster_name    = var.eks_name
+  namespace       = "kube-system"
+  service_account = "cluster-autoscaler"
+  role_arn        = aws_iam_role.cluster_autoscaler.arn
+}
+
+# Deploy the autoscaler the the eks cluster
+resource "helm_release" "cluster_autoscaler" {
+  name = "autoscaler"
+
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart      = "cluster-autoscaler"
+  namespace  = "kube-system"
+  version    = "9.37.0"
+
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "cluster-autoscaler"
+  }
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = var.eks_name
+  }
+
+  # updated the region to match eks region 
+  set {
+    name  = "awsRegion"
+    value = "eu-north-1"
+  }
+
+  depends_on = [helm_release.metrics_server]
+}
---
+++ k8s-addons/pod-identity.tf
@@ -0,0 +1,6 @@
+# Deploy pod identity on cluster using eks addon
+resource "aws_eks_addon" "pod_identity" {
+  cluster_name = var.eks_name
+  addon_name = "eks-pod-identity-agent"
+  addon_version = "v1.3.2-eksbuild.2"
+}
